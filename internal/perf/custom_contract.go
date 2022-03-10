d169df9160ef6e106e1386e9d037466bffa049c0
Nicko Guyer <nguyer@users.noreply.github.com>
2022-03-10 11:29:44 -0500

    Add retrires for looking up blockchain events (#16)
    Signed-off-by: Nicko Guyer <nicko.guyer@kaleido.io>

--- internal/perf/custom_contract.go
+++ internal/perf/custom_contract.go
@@ -2,8 +2,10 @@ package perf
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"strconv"
+	"time"
 
 	"github.com/hyperledger/firefly-perf-cli/internal/conf"
 )
@@ -41,12 +43,24 @@ func (pr *perfRunner) RunCustomContract(id int) {
 	pr.sendAndWait(req, "contracts/invoke", id, conf.PerfCmdCustomContract.String())
 }
 
-func (pr *perfRunner) GetBlockchainEventValue(ref string) (int, error) {
+func (pr *perfRunner) GetBlockchainEventValue(nodeURL, ref string) (res int, err error) {
+	retries := 3
+	for i := 0; i < retries-1; i++ {
+		res, err = pr.getBlockchainEventValue(nodeURL, ref)
+		if err == nil {
+			return res, err
+		}
+		time.Sleep(1000 * time.Millisecond)
+	}
+	return res, err
+}
+
+func (pr *perfRunner) getBlockchainEventValue(nodeURL, ref string) (int, error) {
 	res, err := pr.client.R().
 		SetHeaders(map[string]string{
 			"Accept":       "application/json",
 			"Content-Type": "application/json",
-		}).Get(fmt.Sprintf("%s/api/v1/namespaces/default/blockchainevents/%s", pr.cfg.Node, ref))
+		}).Get(fmt.Sprintf("%s/api/v1/namespaces/default/blockchainevents/%s", nodeURL, ref))
 	if err != nil {
 		return -1, err
 	}
@@ -55,7 +69,13 @@ func (pr *perfRunner) GetBlockchainEventValue(ref string) (int, error) {
 	if err != nil {
 		return -1, err
 	}
-	output := responseBody["output"].(map[string]interface{})
-	value := output["value"].(string)
+	output, ok := responseBody["output"].(map[string]interface{})
+	if !ok {
+		return -1, errors.New("output was nil")
+	}
+	value, ok := output["value"].(string)
+	if !ok {
+		return -1, errors.New("output value was nil")
+	}
 	return strconv.Atoi(value)
 }
