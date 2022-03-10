a4c07162e1f3b1cc434a625b7464c4a41a282b91
Nicko Guyer <nicko.guyer@kaleido.io>
2022-03-10 15:12:13 -0500

    Use enriched events and new filtering
    Signed-off-by: Nicko Guyer <nicko.guyer@kaleido.io>

--- internal/perf/custom_contract.go
+++ internal/perf/custom_contract.go
@@ -1,11 +1,7 @@
 package perf
 
 import (
-	"encoding/json"
-	"errors"
 	"fmt"
-	"strconv"
-	"time"
 
 	"github.com/hyperledger/firefly-perf-cli/internal/conf"
 )
@@ -41,41 +37,4 @@ func (pr *perfRunner) RunCustomContract(id int) {
 		}).
 		SetBody([]byte(payload))
 	pr.sendAndWait(req, "contracts/invoke", id, conf.PerfCmdCustomContract.String())
-}
-
-func (pr *perfRunner) GetBlockchainEventValue(nodeURL, ref string) (res int, err error) {
-	retries := 3
-	for i := 0; i < retries-1; i++ {
-		res, err = pr.getBlockchainEventValue(nodeURL, ref)
-		if err == nil {
-			return res, err
-		}
-		time.Sleep(1000 * time.Millisecond)
-	}
-	return res, err
-}
-
-func (pr *perfRunner) getBlockchainEventValue(nodeURL, ref string) (int, error) {
-	res, err := pr.client.R().
-		SetHeaders(map[string]string{
-			"Accept":       "application/json",
-			"Content-Type": "application/json",
-		}).Get(fmt.Sprintf("%s/api/v1/namespaces/default/blockchainevents/%s", nodeURL, ref))
-	if err != nil {
-		return -1, err
-	}
-	var responseBody map[string]interface{}
-	err = json.Unmarshal(res.Body(), &responseBody)
-	if err != nil {
-		return -1, err
-	}
-	output, ok := responseBody["output"].(map[string]interface{})
-	if !ok {
-		return -1, errors.New("output was nil")
-	}
-	value, ok := output["value"].(string)
-	if !ok {
-		return -1, errors.New("output value was nil")
-	}
-	return strconv.Atoi(value)
 }
