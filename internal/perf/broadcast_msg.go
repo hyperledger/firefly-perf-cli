c4d568dd39f7ae914b16514d13110977952dc5f1
Peter Broadhurst <peter.broadhurst@kaleido.io>
2022-04-15 15:28:08 -0400

    Tests like blob need to do multiple steps on each loop
    Signed-off-by: Peter Broadhurst <peter.broadhurst@kaleido.io>

---
+++ internal/perf/broadcast_msg.go
@@ -0,0 +1,59 @@
+package perf
+
+import (
+	"fmt"
+
+	"github.com/hyperledger/firefly/pkg/fftypes"
+)
+
+type broadcast struct {
+	testBase
+}
+
+func newBroadcastTestWorker(pr *perfRunner, workerID int) TestCase {
+	return &broadcast{
+		testBase: testBase{
+			pr:       pr,
+			workerID: workerID,
+		},
+	}
+}
+
+func (tc *broadcast) Name() string {
+	return "Broadcast Message"
+}
+
+func (tc *broadcast) IDType() TrackingIDType {
+	return TrackingIDTypeMessageID
+}
+
+func (tc *broadcast) RunOnce() (string, error) {
+
+	payload := fmt.Sprintf(`{
+		"data":[
+		   {
+			  "value":{
+				 "broadcastID":"%s"
+			  }
+		   }
+		],
+		"header":{
+		   "tag":"%s"
+		}
+	 }`, tc.getMessageString(tc.pr.cfg.MessageOptions.LongMessage), fmt.Sprintf("%s_%d", tc.pr.tagPrefix, tc.workerID))
+	var resMessage fftypes.Message
+	var resError fftypes.RESTError
+	res, err := tc.pr.client.R().
+		SetHeaders(map[string]string{
+			"Accept":       "application/json",
+			"Content-Type": "application/json",
+		}).
+		SetBody([]byte(payload)).
+		SetResult(&resMessage).
+		SetError(&resError).
+		Post(fmt.Sprintf("%s/api/v1/namespaces/default/messages/broadcast", tc.pr.client.BaseURL))
+	if err != nil || res.IsError() {
+		return "", fmt.Errorf("Error sending broadcast message [%d]: %s (%+v)", resStatus(res), err, &resError)
+	}
+	return resMessage.Header.ID.String(), nil
+}
--- internal/perf/custom_fabric_contract.go
+++ internal/perf/custom_fabric_contract.go
@@ -2,11 +2,33 @@ package perf
 
 import (
 	"fmt"
+	"strconv"
 
-	"github.com/hyperledger/firefly-perf-cli/internal/conf"
+	"github.com/hyperledger/firefly/pkg/fftypes"
 )
 
-func (pr *perfRunner) RunCustomFabricContract(nodeURL string, id int) {
+type customFabric struct {
+	testBase
+}
+
+func newCustomFabricTestWorker(pr *perfRunner, workerID int) TestCase {
+	return &customFabric{
+		testBase: testBase{
+			pr:       pr,
+			workerID: workerID,
+		},
+	}
+}
+
+func (tc *customFabric) Name() string {
+	return "Custom Ethereum"
+}
+
+func (tc *customFabric) IDType() TrackingIDType {
+	return TrackingIDTypeWorkerNumber
+}
+
+func (tc *customFabric) RunOnce() (string, error) {
 	payload := fmt.Sprintf(`{
 		"location": {
 			"channel": "%s",
@@ -30,12 +52,20 @@ func (pr *perfRunner) RunCustomFabricContract(nodeURL string, id int) {
 		"input": {
 			"name": "%v"
 		}
-	}`, pr.cfg.ContractOptions.Channel, pr.cfg.ContractOptions.Chaincode, id)
-	req := pr.client.R().
+	}`, tc.pr.cfg.ContractOptions.Channel, tc.pr.cfg.ContractOptions.Chaincode, tc.workerID)
+	var resContractCall fftypes.ContractCallResponse
+	var resError fftypes.RESTError
+	res, err := tc.pr.client.R().
 		SetHeaders(map[string]string{
 			"Accept":       "application/json",
 			"Content-Type": "application/json",
 		}).
-		SetBody([]byte(payload))
-	pr.sendAndWait(req, nodeURL, "contracts/invoke", id, conf.PerfCmdCustomFabricContract.String())
+		SetBody([]byte(payload)).
+		SetResult(&resContractCall).
+		SetError(&resError).
+		Post(fmt.Sprintf("%s/api/v1/namespaces/default/contracts/invoke", tc.pr.client.BaseURL))
+	if err != nil || res.IsError() {
+		return "", fmt.Errorf("Error invoking contract [%d]: %s (%+v)", resStatus(res), err, &resError)
+	}
+	return strconv.Itoa(tc.workerID), nil
 }
